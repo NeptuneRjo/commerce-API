0fe7831745c9214ea72ff1d9538d50cf3cc2b5c2
NeptuneRjo <rjo9000@outlook.com>
2023-07-14 19:27:37 -0400

    Create datacontext interface

---
+++ Interfaces/IDataContext.cs
@@ -0,0 +1,16 @@
+﻿using CommerceClone.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceClone.Interfaces
+{
+    public interface IDataContext
+    {
+        DbSet<Admin> Admins { get; set; }
+        DbSet<Cart> Carts { get; set; }
+        DbSet<Item> Items { get; set; }
+        DbSet<Store> Stores { get; set; }
+        DbSet<User> Users { get; set; }
+
+        int SaveChanges();
+    }
+}
