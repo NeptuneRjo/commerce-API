9801ee2bca43c2e7104a7575f7e8e84edb0f1efb
NeptuneRjo <rjo9000@outlook.com>
2023-07-14 15:07:59 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommerceClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
