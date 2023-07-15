164de9f69b6e585ba3a6145e37d2c156fc140337
NeptuneRjo <rjo9000@outlook.com>
2023-07-15 06:36:54 -0400

    Create Admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using CommerceClone.Models;
+using CommerceClone.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceClone.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AdminRepository _repository;
+
+        public AdminController(AdminRepository adminRepository)
+        {
+            _repository = adminRepository;
+        }
+
+        [HttpPost("~/api/admin/create")]
+        public void InsertAdmin(Admin admin)
+        {
+            _repository.Add(admin);
+        }
+
+        [HttpGet("~/api/admin/all")]
+        public ICollection<Admin> GetAllAdmins()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("~/api/admin/{query}")]
+        public Admin GetAdmin(string query)
+        {
+            if (Int32.TryParse(query, out int i)) {
+                return _repository.GetById(i);
+            }
+
+            return _repository.GetAdminByUsername(query);
+        }
+
+        [HttpPatch("~/api/admin/update")]
+        public void UpdateAdmin(Admin admin)
+        {
+            _repository.Update(admin);
+        }
+
+        [HttpDelete("~/api/admin/delete")]
+        public void DeleteAdmin(Admin admin)
+        {
+            _repository.Delete(admin);
+        }
+    }
+}
