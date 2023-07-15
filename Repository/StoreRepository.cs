4dbc32f4547e0221208635bc171311ada5b3d016
NeptuneRjo <rjo9000@outlook.com>
2023-07-15 05:06:06 -0400

    Create store repository

---
+++ Repository/StoreRepository.cs
@@ -0,0 +1,29 @@
+﻿using CommerceClone.Interfaces;
+using CommerceClone.Models;
+
+namespace CommerceClone.Repository
+{
+    public class StoreRepository : Repository<Store>, IStoreRepository
+    {
+        private readonly IDataContext _context;
+
+        public StoreRepository(IDataContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Store> GetStoresByAdmin(Admin admin)
+        {
+            return _context.Stores
+                .Where(e => e.Admin == admin)
+                .ToList();
+        }
+
+        public IEnumerable<Store> GetStoresByAdmin(string username)
+        {
+            return _context.Stores
+                .Where(e => e.Admin.Username == username)
+                .ToList();
+        }
+    }
+}
