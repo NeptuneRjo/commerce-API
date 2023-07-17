e26d8d6516f7ca0d2845c9713a3e675a8f360606
NeptuneRjo <rjo9000@outlook.com>
2023-07-17 18:21:52 -0400

    Update to match api standards

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,14 +1,17 @@
-﻿using CommerceClone.Models;
+﻿using CommerceClone.Interfaces;
+using CommerceClone.Models;
 using CommerceClone.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommerceClone.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]/[action]/{query?}")]
     public class AdminController : ControllerBase
     {
-        private AdminRepository _repository;
+        private IAdminRepository _repository;
 
-        public AdminController(AdminRepository adminRepository)
+        public AdminController(IAdminRepository adminRepository)
         {
             _repository = adminRepository;
         }
@@ -32,15 +35,16 @@ namespace CommerceClone.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<Admin> GetAdmin(string query)
+        public ActionResult<Admin> Get(string query)
         {
             try
             {
                 if (Int32.TryParse(query, out int i))
+                {
                     return Ok(_repository.GetById(i));
-                
+                }
 
-                return Ok(_repository.GetAdminByUsername(query));
+                return Ok(_repository.GetAdminByEmail(query));   
             }
             catch (Exception ex)
             {
@@ -51,11 +55,11 @@ namespace CommerceClone.Controllers
         [HttpPatch]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult UpdateAdmin(Admin admin)
+        public ActionResult Update(int query, Admin admin)
         {
             try
             {
-                _repository.Update(admin);
+                _repository.Update(query, admin);
                 return Ok();
             }
             catch (Exception ex)
@@ -67,11 +71,11 @@ namespace CommerceClone.Controllers
         [HttpDelete]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult DeleteAdmin(int id)
+        public ActionResult Delete(int query)
         {
             try
             {
-                _repository.Delete(id);
+                _repository.Delete(query);
                 return Ok();
             }
             catch (Exception ex)
