09c55818d6a9eb93331caffac9f568459c7c8c8a
NeptuneRjo <rjo9000@outlook.com>
2023-07-17 18:22:42 -0400

    Change username to email and create empty list

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -3,9 +3,14 @@
     public class Admin
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
 
         public ICollection<Store> Stores { get; set; }
+
+        public Admin()
+        {
+            Stores = new List<Store>();
+        }
     }
 }
