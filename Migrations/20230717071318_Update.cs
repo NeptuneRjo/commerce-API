b6311d11ca690b45eac499031c072921538b1c6d
NeptuneRjo <rjo9000@outlook.com>
2023-07-17 18:23:17 -0400

    Migrations

---
+++ Migrations/20230717071318_Update.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommerceClone.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Oauth",
+                table: "Admins");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Admins",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Admins",
+                newName: "Username");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Oauth",
+                table: "Admins",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
