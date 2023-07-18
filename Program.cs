4366da682ed65c2238f209040e618172f5a10b20
NeptuneRjo <rjo9000@outlook.com>
2023-07-18 18:50:21 -0400

    Remove unused scope; add authentication

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using CommerceClone.Data;
 using CommerceClone.Interfaces;
 using CommerceClone.Repository;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,17 +10,37 @@ IServiceCollection services = builder.Services;
 // Add services to the container.
 services.AddControllersWithViews();
 
-services.AddScoped<IAdminRepository, AdminRepository>();
 services.AddScoped<ICartRepository, CartRepository>();
 services.AddScoped<IItemRepository, ItemRepository>();
 services.AddScoped<IStoreRepository, StoreRepository>();
 services.AddScoped<IUserRepository, UserRepository>();
 services.AddScoped<IDataContext, DataContext>();
+services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
 
 services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+services.AddAuthentication(opt =>
+{
+    opt.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+    .AddCookie(opt =>
+    {
+        opt.LoginPath = "/signin";
+        opt.LogoutPath = "/signout";
+    })
+    .AddGitHub(opt =>
+    {
+        opt.ClientId = builder.Configuration["GitHub:ClientId"];
+        opt.ClientSecret = builder.Configuration["GitHub:ClientSecret"];
+    });
+    //.AddGoogle(opt =>
+    //{
+    //    opt.ClientId = builder.Configuration["Google:ClientId"];
+    //    opt.ClientSecret = builder.Configuration["Google:ClientSecret"];
+    //});
 
 var app = builder.Build();
 
@@ -36,6 +57,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
