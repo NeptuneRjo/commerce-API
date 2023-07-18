fefd07f214ca62a1fd541bc2e9d90df7d29ce58e
NeptuneRjo <rjo9000@outlook.com>
2023-07-18 01:05:49 -0400

    Add authentication controller for oauth

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using CommerceClone.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceClone.Controllers
+{
+    [ApiController]
+    [Route("api/auth/[action]/{query?}")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> Providers()
+        {
+            return Ok(await HttpContext.GetExternalProvidersAsync());
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> SignIn([FromForm] string provider)
+        {
+            if (string.IsNullOrWhiteSpace(provider))
+                return BadRequest();
+
+            if (!await HttpContext.IsProviderSupportedAsync(provider))
+                return BadRequest();
+
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
+        }
+
+        [HttpGet]
+        [HttpPost]
+        public ActionResult SignOutUser()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = "/" },
+                CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
