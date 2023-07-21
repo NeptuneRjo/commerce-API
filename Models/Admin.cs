10202cd3a9f440e4f428e09d8e99a7eab7a7be76
NeptuneRjo <rjo9000@outlook.com>
2023-07-20 23:10:59 -0400

    Add keys

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -5,7 +5,14 @@
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string SecretKey { get; set; }
+        public string PublicKey { get; set; }
 
         public ICollection<Store> Stores { get; set; }
+
+        public Admin()
+        {
+            Stores = new List<Store>();
+        }
     }
 }
