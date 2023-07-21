ea45226472f7892e0c84815e37331aff143209ef
NeptuneRjo <rjo9000@outlook.com>
2023-07-20 23:10:49 -0400

    Added description and initialize empty list for items on creation

--- Models/Store.cs
+++ Models/Store.cs
@@ -4,11 +4,16 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ConnectionString { get; set; }
+        public string Description { get; set; }
 
         public ICollection<Item> Items { get; set; }
 
         public Admin Admin { get; set; }
         public int AdminId { get; set; }
+
+        public Store()
+        {
+            Items = new List<Item>();
+        }
     }
 }
