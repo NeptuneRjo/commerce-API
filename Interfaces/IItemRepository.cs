93e294f0f9eb3086524cb6bd04038c9de63be53b
NeptuneRjo <rjo9000@outlook.com>
2023-07-21 02:53:26 -0400

    Update item repo

--- Interfaces/IItemRepository.cs
+++ Interfaces/IItemRepository.cs
@@ -2,8 +2,10 @@
 
 namespace CommerceClone.Interfaces
 {
-    public interface IItemRepository
+    public interface IItemRepository : IRepository<Item>
     {
         Item GetItemByName(string name);
+        ICollection<Item> GetByStore(int storeId, string pk);
+        void AddToStore(string sk, int storeId, Item item);
     }
 }
--- Repository/ItemRepository.cs
+++ Repository/ItemRepository.cs
@@ -12,9 +12,37 @@ namespace CommerceClone.Repository
             _context = context;
         }
 
+        public void AddToStore(string sk, int storeId, Item item)
+        {
+            var store = _context.Stores
+                .Where(e => e.Admin.SecretKey == sk)
+                .FirstOrDefault(e => e.Id == storeId);
+                
+
+            _context.Items.Add(item);
+            store.Items.Add(item);
+
+            _context.SaveChanges();
+        }
+
+        public ICollection<Item> GetByStore(int storeId, string pk)
+        {
+            return _context.Items
+                .Where(e => e.Store.Admin.PublicKey == pk)
+                .Where(e => e.StoreId == storeId).ToList();
+        }
+
         public Item GetItemByName(string name)
         {
             return _context.Items.Find(name);
+        }
+
+        public Item GetById(string pk, int storeId, int itemId)
+        {
+            return _context.Items
+                .Where(e => e.Store.Admin.PublicKey == pk)
+                .Where(e => e.StoreId == storeId)
+                .FirstOrDefault(e => e.Id == itemId);
         }
     }
 }
