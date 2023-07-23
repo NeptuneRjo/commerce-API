b289140a2217c4548075a1d2eb8b31d4f7770a31
NeptuneRjo <rjo9000@outlook.com>
2023-07-22 22:05:53 -0400

    Add cart item

---
+++ Models/CartItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommerceClone.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public string Id { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+
+        public Cart Cart { get; set; }
+        public string CartId { get; set; }
+
+        public CartItem()
+        {
+            Total = Item.Price * Quantity;
+        }
+    }
+}
