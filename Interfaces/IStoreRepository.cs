d0ac3ebd62f4f9753812456023a5b380657df3b6
NeptuneRjo <rjo9000@outlook.com>
2023-07-26 19:56:27 -0400

    Add store methods

--- Interfaces/IStoreRepository.cs
+++ Interfaces/IStoreRepository.cs
@@ -1,11 +1,27 @@
-﻿using CommerceClone.Models;
+﻿using CommerceClone.DTO;
+using CommerceClone.Models;
 
 namespace CommerceClone.Interfaces
 {
     public interface IStoreRepository : IRepository<Store>
     {
-        //void AddToAdmin(string key, Store store);
-        //ICollection<Store> GetAllByKey(string key);
+        /// <summary>
+        /// Creates a new store using an admin's secret key
+        /// </summary>
+        /// <param name="sk"></param>
+        /// <param name="store"></param>
+        /// <returns>The store object after creation</returns>
         Store AddByKey(string sk, Store store);
+        Store AddByEmail(string email, Store store);
+        Store AddByAdmin(Admin admin, Store store);
+        ICollection<Store> GetAllByAdminId(int id);
+        ICollection<Store> GetAllByPk(string pk);
+        ICollection<Store> GetAllByEmail(string email);
+        /// <summary>
+        /// Store-specific method for retrieving the store with the Admin reference
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        new Store GetById(int id);
     }
 }
--- Repository/StoreRepository.cs
+++ Repository/StoreRepository.cs
@@ -1,27 +1,81 @@
-﻿using CommerceClone.Interfaces;
+﻿using AutoMapper;
+using CommerceClone.DTO;
+using CommerceClone.Interfaces;
 using CommerceClone.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommerceClone.Repository
 {
     public class StoreRepository : Repository<Store>, IStoreRepository
     {
         private readonly IDataContext _context;
+        private readonly IMapper _mapper;
 
-        public StoreRepository(IDataContext context) : base(context)
+        public StoreRepository(IDataContext context, IMapper mapper) : base(context, mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public Store AddByKey(string sk, Store store)
+        public Store AddByAdmin(Admin admin, Store store)
         {
-            var admin = _context.Admins.First(e => e.SecretKey == sk);
+            store.Admin = admin;
 
-            store.AdminId = admin.Id;
+            _context.Stores.Add(store);
+
             admin.Stores.Add(store);
 
             _context.SaveChanges();
 
             return store;
+        }
+
+        public Store AddByEmail(string email, Store store)
+        {
+            Admin admin = _context.Admins.First(e => e.Email == email);
+
+            store.Admin = admin;
+
+            _context.Stores.Add(store);
+            _context.SaveChanges();
+
+            return store;
+        }
+
+        public Store AddByKey(string sk, Store store)
+        {
+            var admin = _context.Admins.FirstOrDefault(e => e.SecretKey == sk);
+
+            store.Admin = admin;
+
+            _context.Stores.Add(store);
+
+            admin.Stores.Add(store);
+
+            _context.SaveChanges();
+
+            return store;
+        }
+
+        public ICollection<Store> GetAllByAdminId(int id)
+        {
+            return _context.Stores.Where(e => e.AdminId == id).ToList();
+        }
+
+        public ICollection<Store> GetAllByEmail(string email)
+        {
+            return _context.Stores.Where(e => e.Admin.Email == email).ToList();
+        }
+
+        public ICollection<Store> GetAllByPk(string pk)
+        {
+            return _context.Stores.Where(e => e.Admin.PublicKey == pk).ToList();
+        }
+
+        new public Store GetById(int id)
+        {
+            return _context.Stores.Include(e => e.Admin)
+                .SingleOrDefault(e => e.Id == id);
         }
     }
 }
