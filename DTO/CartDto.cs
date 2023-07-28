b43d31e2fe2161236cadc70092efd40c0c7410f9
NeptuneRjo <rjo9000@outlook.com>
2023-07-28 04:00:45 -0400

    Add store id

--- DTO/CartDto.cs
+++ DTO/CartDto.cs
@@ -14,6 +14,7 @@ namespace CommerceClone.DTO
 
         [JsonPropertyName("cart_items")]
         public ICollection<CartItem> CartItems { get; set; }
+        public int StoreId { get; set; }
 
         public CartDto()
         {
