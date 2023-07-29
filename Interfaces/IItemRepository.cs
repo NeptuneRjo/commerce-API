1cd6b9f35ba266191478283d85cbc5b9f21cb675
NeptuneRjo <rjo9000@outlook.com>
2023-07-29 01:25:25 -0400

    Add create methods

--- Interfaces/IItemRepository.cs
+++ Interfaces/IItemRepository.cs
@@ -4,5 +4,7 @@ namespace CommerceClone.Interfaces
 {
     public interface IItemRepository : IRepository<Item>
     {
+        Item AddByKey(string sk, int storeId, Item item);
+        Item AddByEmail(string email, int storeId, Item item);
     }
 }
