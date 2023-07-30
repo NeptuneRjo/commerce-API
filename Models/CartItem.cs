df34d1feffd4d926050e1c72e1282faf2bf022df
NeptuneRjo <rjo9000@outlook.com>
2023-07-30 00:09:26 -0400

    Remove unecessary initialization; add json property names

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,21 +1,19 @@
-﻿namespace CommerceClone.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CommerceClone.Models
 {
     public class CartItem
     {
         public int Id { get; set; }
 
-        public int CartId { get; set; }
+        [JsonPropertyName("item_id")]
         public int ItemId { get; set; }
 
+        [JsonPropertyName("cart_id")]
+        public int CartId { get; set; }
         public Cart Cart { get; set; }
-        public Item Item { get; set; }
 
         public int Quantity { get; set; }
-        public decimal Total { get; set; }
-
-        public CartItem()
-        {
-            Total = Item.Price * Quantity;
-        }
+        public decimal? Total { get; set; }
     }
 }
