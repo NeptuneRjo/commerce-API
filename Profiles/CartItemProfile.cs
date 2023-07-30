66e1945f1b698ec7af1210bed61111516ac95baf
NeptuneRjo <rjo9000@outlook.com>
2023-07-30 00:09:39 -0400

    Create cartitem profile

---
+++ Profiles/CartItemProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CommerceClone.DTO;
+using CommerceClone.Models;
+
+namespace CommerceClone.Profiles
+{
+    public class CartItemProfile : Profile
+    {
+        public CartItemProfile()
+        {
+            CreateMap<UpdateCartModel, CartItem>();
+            CreateMap<CartItem, CartItemDto>();
+        }
+    }
+}
