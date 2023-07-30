3ce840b55be4fc661f3c431ed317c1b047b2cdba
NeptuneRjo <rjo9000@outlook.com>
2023-07-30 00:07:25 -0400

    Migrations

--- Migrations/20230729053446_CartItem.Designer.cs
+++ Migrations/20230729053446_CartItem.Designer.cs
@@ -4,6 +4,7 @@ using CommerceClone.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CommerceClone.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230729053446_CartItem")]
+    partial class CartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -64,8 +67,8 @@ namespace CommerceClone.Migrations
                     b.Property<int>("StoreId")
                         .HasColumnType("int");
 
-                    b.Property<decimal?>("Subtotal")
-                        .HasColumnType("decimal(18,2)");
+                    b.Property<string>("Subtotal")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<int?>("TotalItems")
                         .HasColumnType("int")
