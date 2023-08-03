74119c7bbc6db86d20dc8f33fe8473615ff6e83b
NeptuneRjo <rjo9000@outlook.com>
2023-08-02 20:33:04 -0400

    Implement item service

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,71 +1,60 @@
-﻿using CommerceClone.DTO;
+﻿using CommerceClone.CustomExceptions;
+using CommerceClone.DTO;
 using CommerceClone.Interfaces;
 using CommerceClone.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq.Expressions;
-using System.Security.Claims;
 
 namespace CommerceClone.Controllers
 {
     [ApiController]
     [Route("v1/items")]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(500)]
     public class ItemController : ControllerBase
     {
-        private readonly IItemRepository _item;
+        private readonly IItemService _service;
 
-        private readonly Expression<Func<Item, object>>[] includes = { e => e.Store.Admin, e => e.Store };
-
-        public ItemController(IItemRepository item)
+        public ItemController(IItemService service)
         {
-            _item = item;
+            _service = service;
         }
 
         // GET: v1/items/{item_id}
+        // Takes Public or Secret Key
         [HttpGet("{itemId}")]
         [AllowAnonymous]
         [ProducesResponseType(200, Type = typeof(ItemDto))]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        [ProducesResponseType(404)]
         public ActionResult GetItemById(int itemId)
         {
             try
             {
                 string key = Request.Headers["X-Authorization"];
-                string email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-                Item item = _item.GetByQuery(e => e.Id == itemId, includes);
-                //Store store = _store.
-
-                if (item == null)
-                    return NotFound();
-
-                if (!string.IsNullOrEmpty(key) && item.Store.Admin.PublicKey != key)
-                    return Unauthorized();
-                // If email is defined, check auth
-                if (!string.IsNullOrEmpty(email) && item.Store.Admin.Email != email)
-                    return Unauthorized();
-
-                Console.WriteLine(item.Store.Admin.Email);
-
-                ItemDto dto = _item.Map<ItemDto>(item);
+                ItemDto dto = _service.GetItemById(key, itemId);
 
                 return Ok(dto);
             }
+            catch (ObjectNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return StatusCode(500, ex.Message);
             }
         }
 
         // PUT: v1/items/{item_id}
+        // Takes Secret Key
         [HttpPut("{itemId}")]
         [AllowAnonymous]
         [ProducesResponseType(200, Type = typeof(ItemDto))]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        [ProducesResponseType(404)]
         public ActionResult UpdateItem(int itemId, ItemModelUpdate update)
         {
             if (!ModelState.IsValid)
@@ -74,71 +63,54 @@ namespace CommerceClone.Controllers
             try
             {
                 string key = Request.Headers["X-Authorization"];
-                string email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-                Item item = _item.GetByQuery(e => e.Id == itemId, includes);
-
-                if (item == null)
-                    return NotFound();
-
-                if (!string.IsNullOrEmpty(key) && item.Store.Admin.SecretKey != key)
-                    return Unauthorized();
-                if (!string.IsNullOrEmpty(email) && item.Store.Admin.Email != email)
-                    return Unauthorized();
-
-                if (update.Name != null)
-                    item.Name = update.Name;
-                if (update.Description != null)
-                    item.Description = update.Description;
-                if (update.Image != null)
-                    item.Image = update.Image;
-                if (update.Count.HasValue)
-                    item.Count = update.Count.Value;
-
-                _item.Update(itemId, item);
-
-                ItemDto dto = _item.Map<ItemDto>(item);
+                ItemDto dto = _service.UpdateItem(key, itemId, update);
 
                 return Ok(dto);
             }
+            catch (ObjectNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
-                return BadRequest(ex.Message);
+                return StatusCode(500, ex.Message);
             }
         }
 
         // DELETE: v1/items/{item_id}
+        // Takes Secret Key
         [HttpDelete("{itemId}")]
         [AllowAnonymous]
         [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        [ProducesResponseType(404)]
         public ActionResult DeleteItem(int itemId)
         {
             try
             {
                 string key = Request.Headers["X-Authorization"];
-                string email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-                Item item = _item.GetByQuery(e => e.Id == itemId, includes);
+                bool deleted = _service.DeleteItem(key, itemId);
 
-                if (item == null)
-                    return NotFound();
-
-                if (!string.IsNullOrEmpty(key) && item.Store.Admin.SecretKey != key)
-                    return Unauthorized();
-                if (!string.IsNullOrEmpty(email) && item.Store.Admin.Email != email)
-                    return Unauthorized();
-
-                _item.Delete(itemId);
+                if (!deleted)
+                    throw new Exception("Failed to delete item");
 
                 return Ok();
             }
+            catch (ObjectNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return StatusCode(500, ex.Message);
             }
         }
 
