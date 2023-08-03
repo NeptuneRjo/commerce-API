fe4e462b3e65dff43dbe9618be7845558db11a66
NeptuneRjo <rjo9000@outlook.com>
2023-08-02 20:33:38 -0400

    Create item service

---
+++ Interfaces/IItemService.cs
@@ -0,0 +1,31 @@
+﻿using CommerceClone.DTO;
+using CommerceClone.Models;
+
+namespace CommerceClone.Interfaces
+{
+    public interface IItemService
+    {
+        /// <summary>
+        /// Queries the database for the item that matches the id
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="itemId"></param>
+        /// <returns>The found <see cref="ItemDto"/> object</returns>
+        ItemDto GetItemById(string key, int itemId);
+        /// <summary>
+        /// Queries the database for the item that matches the id and updates its properties
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="itemId"></param>
+        /// <param name="update"></param>
+        /// <returns>The updated <see cref="ItemDto"/> object</returns>
+        ItemDto UpdateItem(string secrretKey, int itemId, ItemModelUpdate update);
+        /// <summary>
+        /// Queries the database for the item that matches the id and deletes it
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="itemId"></param>
+        /// <returns>true if the item is successfully deleted</returns>
+        bool DeleteItem(string secretKey, int itemId);
+    }
+}
