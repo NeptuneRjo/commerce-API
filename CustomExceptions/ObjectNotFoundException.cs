4dba1c75108e68298a837c2f9370984006f3b83a
NeptuneRjo <rjo9000@outlook.com>
2023-08-02 20:33:30 -0400

    Create custom exception for when an object is not found

---
+++ CustomExceptions/ObjectNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace CommerceClone.CustomExceptions
+{
+    public class ObjectNotFoundException : Exception
+    {
+        public ObjectNotFoundException() { }
+
+        public ObjectNotFoundException(string message)
+            : base(message) { }
+
+        public ObjectNotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
