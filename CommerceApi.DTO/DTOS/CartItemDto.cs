5491f5ecfe5117dff64639fe463323e4e8e9f5fc
NeptuneRjo <rjo9000@outlook.com>
2023-09-06 04:08:32 -0400

    Create dto layer

---
+++ CommerceApi.DTO/DTOS/CartItemDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CommerceApi.DTO.DTOS
+{
+    public class CartItemDto
+    {
+        public int Id { get; set; }
+
+        [JsonPropertyName("item_id")]
+        public int ItemId { get; set; }
+
+        [JsonPropertyName("cart_id")]
+        public int CartId { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal? Total { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
