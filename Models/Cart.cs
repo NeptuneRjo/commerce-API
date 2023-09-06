6d3af3932e34633da7a7680ad8f7946520c7cf84
NeptuneRjo <rjo9000@outlook.com>
2023-09-06 00:26:59 -0400

    Update cart model

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -1,38 +1,21 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CommerceClone.Models
 {
     public class Cart
     {
-        public int Id { get; set; }
-        [JsonPropertyName("total_items")]
-        public int? TotalItems { get; set; }
-        [JsonPropertyName("total_unique_items")]
-        public int? TotalUniqueItems { get; set; }
-        public decimal? Subtotal { get; set; }
+        [Key]
+        public int CartId { get; set; }
 
-        [JsonPropertyName("cart_items")]
-        public ICollection<CartItem>? CartItems { get; set; }
+        public int TotalItems { get; set; }
 
-        public Cart()
-        {
-            CartItems = new List<CartItem>();
+        public int TotalUniqueItems { get; set; }
 
-            TotalItems = CartItems.Count();
+        public decimal Subtotal { get; set; }
 
-            TotalUniqueItems = CartItems.GroupBy(e => e.ItemId)
-                .Select(group => new { Id = group.Key, Count = group.Count() })
-                .ToList()
-                .Count();
+        public ICollection<CartProduct> CartProducts { get; set; }
 
-            Subtotal = CartItems.Sum(e => e.Total);
-        }
-    }
-
-    public class UpdateCartModel
-    {
-        [JsonPropertyName("item_id")]
-        public int ItemId { get; set; }
-        public int Quantity { get; set; } = 1;
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
