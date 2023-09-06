d1c1f6b3a88ac3435e6d848f0a0b46542988b44b
NeptuneRjo <rjo9000@outlook.com>
2023-09-05 23:58:07 -0400

    Del cartitem and error view model

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CommerceClone.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
