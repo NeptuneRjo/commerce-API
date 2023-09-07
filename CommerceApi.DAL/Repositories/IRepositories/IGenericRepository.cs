9b96a3d9e492598762fd626f5207179f440d232b
NeptuneRjo <rjo9000@outlook.com>
2023-09-06 23:48:18 -0400

    Add documentation and update methods to use async/await

--- CommerceApi.DAL/Repositories/IRepositories/IGenericRepository.cs
+++ CommerceApi.DAL/Repositories/IRepositories/IGenericRepository.cs
@@ -4,19 +4,61 @@ namespace CommerceApi.DAL.Repositories
 {
     public interface IGenericRepository<TEntity>
     {
-        TEntity Add(TEntity entity);
-        void AddRange(IEnumerable<TEntity> entities);
-        ICollection<TEntity> GetAll();
         /// <summary>
-        /// Query the database. Include any reference objects.
+        /// Add an entity to the database asynchronously
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>Returns the added <see cref="TEntity"/></returns>
+        Task<TEntity> Add(TEntity entity);
+
+        /// <summary>
+        /// Add a collection of entities to the database asynchronously
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns>Returns the collection of added <see cref="ICollection{TEntity}"/></returns>
+        Task<ICollection<TEntity>> AddRange(ICollection<TEntity> entities);
+
+        /// <summary>
+        /// Retrieves a collection of entities asynchronously
+        /// </summary>
+        /// <returns>The retrieved <see cref="ICollection{TEntity}"/></returns>
+        Task<ICollection<TEntity>> GetAll();
+
+        /// <summary>
+        /// Retrieve an entity by a query asynchronously
         /// </summary>
         /// <param name="predicate"></param>
         /// <param name="includes"></param>
-        /// <returns>The found entity or null</returns>
-        TEntity GetByQuery(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
-        ICollection<TEntity> GetAllByQuery(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
-        void Update(int id, TEntity entity);
-        void Delete(int id);
-        void SaveChanges();
+        /// <returns>The retrieved <see cref="TEntity"/></returns>
+        Task<TEntity> GetByQuery(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
+
+        /// <summary>
+        /// Retrieve a collection of entities by a query asynchronously
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="includes"></param>
+        /// <returns>The retrieved <see cref="ICollection{TEntity}"/></returns>
+        Task<ICollection<TEntity>> GetAllByQuery(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
+        
+        /// <summary>
+        /// Update an entity asynchronously
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="entity"></param>
+        /// <returns>The updated <see cref="TEntity"/></returns>
+        Task<TEntity> Update(int id, TEntity entity);
+
+        /// <summary>
+        /// Delete an entity asynchronously
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task Delete(int id);
+
+        /// <summary>
+        /// Save the context's changes asynchronously
+        /// </summary>
+        /// <returns></returns>
+        Task SaveChanges();
     }
 }
