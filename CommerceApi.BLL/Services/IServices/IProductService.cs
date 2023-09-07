371b6180c990ce1e3897e0b8ed05ef021986623b
NeptuneRjo <rjo9000@outlook.com>
2023-09-07 03:57:12 -0400

    Define product service methods

---
+++ CommerceApi.BLL/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using CommerceApi.DTO.DTOS;
+
+namespace CommerceApi.BLL.Services
+{
+    public interface IProductService
+    {
+        Task<ICollection<ProductDto>> GetProductsAsync();
+
+        Task<ProductDto> GetProductAsync(string id);
+
+        Task<ProductDto> AddProductAsync(ProductToAddDto productToAddDto);
+
+        Task<ProductDto> UpdateProductAsync(ProductToUpdateDto productToUpdateDto);
+
+        Task DeleteProductAsync(string id);
+    }
+}
