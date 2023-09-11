37e27f15e7b6856fc55bb5793ba9779e8e38f51d
NeptuneRjo <rjo9000@outlook.com>
2023-09-11 02:17:08 -0400

    Define interface for generic service class

---
+++ CommerceApi.BLL/Services/IServices/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace CommerceApi.BLL.Services
+{
+    public interface IGenericService<TEntity>
+    {
+        Task<IGenericService<TEntity>> GetEntitiesAsync();
+        Task<IGenericService<TEntity>> GetEntityAsync(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, object>>[] includes);
+        Task<IGenericService<TEntity>> AddEntityAsync(TEntity entity);
+        Task<IGenericService<TEntity>> AddEntitiesAsync(IList<TEntity> entities);
+        Task<IGenericService<TEntity>> UpdateEntityAsync(TEntity entity);
+        Task DeleteEntityAsync(TEntity entity);
+
+        IGenericService<TEntity> MapEntity<TSource, TDestination>(TSource source);
+        IGenericService<TEntity> MapEntities<TSource, TDestination>(IList<TSource> sourceList);
+    }
+}
