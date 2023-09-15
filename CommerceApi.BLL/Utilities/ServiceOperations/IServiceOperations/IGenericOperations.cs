fc6d716ec13f6cba54e59436e0ab4a49e2e7cd52
NeptuneRjo <rjo9000@outlook.com>
2023-09-15 01:56:33 -0400

    Define interface for generic operations

---
+++ CommerceApi.BLL/Utilities/ServiceOperations/IServiceOperations/IGenericOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceApi.BLL.Utilities
+{
+    public interface IGenericOperations<TEntity>
+    {
+        Task<TEntity> RetrieveEntityOperation(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, object>>[]? includes = null);
+
+        Task<ICollection<TEntity>> RetrieveEntitiesOperation();
+
+        Task<TEntity> UpdateEntityOperation(TEntity entity);
+
+        Task DeleteEntityOperation(Expression<Func<TEntity, bool>> filter);
+
+        Task<TEntity> AddEntityOperation(TEntity entity);
+    }
+}
