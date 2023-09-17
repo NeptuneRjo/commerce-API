b385481b770b909c012942e0c08acfe5d912ce80
NeptuneRjo <rjo9000@outlook.com>
2023-09-17 01:00:24 -0400

    Define interface for product operations

---
+++ CommerceApi.BLL/Utilities/Operations/IOperations/IProductOperations.cs
@@ -0,0 +1,11 @@
+﻿using CommerceApi.DAL.Entities;
+using CommerceApi.DTO.DTOS;
+
+namespace CommerceApi.BLL.Utilities
+{
+    public interface IProductOperations : IGenericOperations<Product>
+    {
+        Task<Product> AddProductOperation(Product productToAdd);
+
+    }
+}
