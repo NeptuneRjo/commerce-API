de3c49dc06dbf2deb66936e65b277fe40e161a59
NeptuneRjo <rjo9000@outlook.com>
2023-09-17 01:01:08 -0400

    Create product services

--- CommerceApi.BLL/Services/ProductService.cs
+++ CommerceApi.BLL/Services/ProductService.cs
@@ -1,101 +1,25 @@
 ﻿using AutoMapper;
 using CommerceApi.DAL.Entities;
-using CommerceApi.DAL.Repositories;
 using CommerceApi.DTO.DTOS;
 using CommerceApi.BLL.Utilities;
-using Microsoft.Extensions.Logging;
-using System.Linq.Expressions;
 
 namespace CommerceApi.BLL.Services
 {
-    public class ProductService : IProductService
+    public class ProductService : GenericService<Product>, IProductService
     {
-        private readonly IProductRepository _repository;
         private readonly IMapper _mapper;
-        private readonly ILogger<ProductService> _logger;
+        private readonly IProductOperations _ops;
 
-        public ProductService(IProductRepository repository, IMapper mapper, ILogger<ProductService> logger)
+        public ProductService(IMapper mapper, IProductOperations ops) : base(mapper, ops)
         {
             _mapper = mapper;
-            _repository = repository;
-            _logger = logger;
+            _ops = ops;
         }
 
-        public async Task<ICollection<ProductDto>> GetProductsAsync()
-        {
-            ICollection<Product> products = await _repository.GetAll();
+        public async Task<ProductDto> AddProductAsync(ProductToAddDto productToAddDto) =>
+            _mapper.Map<ProductDto>(await _ops.AddProductOperation(_mapper.Map<Product>(productToAddDto)));
 
-            _logger.LogInformation($"List of {products.Count} products has been returned");
-
-            return _mapper.Map<ICollection<ProductDto>>(products); ;
-        }
-
-        public async Task<ProductDto> GetProductAsync(string id)
-        {
-            _logger.LogInformation($"Product with the ProductId = {id} was requested");
-
-            Expression<Func<Product, bool>> query = e => e.ProductId == id;
-            Expression<Func<Product, object>>[] includes = { e => e.ProductReviews };
-
-            Product product = await _repository.GetProductAsync(id);
-
-            if (product is null)
-            {
-                _logger.LogError($"Product with the ProductId = {id} was not found");
-                throw new NotFoundException();
-            }
-
-            return _mapper.Map<ProductDto>(product);
-        }
-
-        public async Task<ProductDto> AddProductAsync(ProductToAddDto productToAddDto)
-        {
-            Product product = _mapper.Map<Product>(productToAddDto);
-
-            product.CreatedAt = DateTime.Now;
-            product.UpdatedAt = DateTime.Now;
-
-            product.ProductReviews = new List<ProductReview>();
-            product.CartProducts = new List<CartProduct>();
-            product.WishListProducts = new List<WishListProduct>();
-
-            Product addedProduct = await _repository.Add(product);
-
-            _logger.LogInformation($"Product with the properties: {addedProduct} was added");
-
-            return _mapper.Map<ProductDto>(addedProduct);
-        }
-
-        public async Task<ProductDto> UpdateProductAsync(ProductToUpdateDto productToUpdateDto)
-        {
-            Product product = await _repository.GetProductAsync(productToUpdateDto.ProductId);
-
-            if (product is null)
-            {
-                _logger.LogError($"Product with the ProductId = {productToUpdateDto.ProductId} was not found");
-                throw new NotFoundException();
-            }
-
-            Product productToUpdate = _mapper.Map<Product>(productToUpdateDto);
-
-            _logger.LogInformation($"Product with these properties: {productToUpdateDto} has been updated");
-
-            Product updatedProduct = await _repository.Update(productToUpdate);
-
-            return _mapper.Map<ProductDto>(updatedProduct);
-        }
-
-        public async Task DeleteProductAsync(string id)
-        {
-            Product productToDelete = await _repository.GetProductAsync(id);
-
-            if (productToDelete is null) 
-            {
-                _logger.LogError($"Product with the ProductId = {id} was not found");
-                throw new NotFoundException();
-            }
-
-            await _repository.Delete(productToDelete);
-        }
+        public async Task<ProductDto> UpdateProductAsync(string id, ProductToUpdateDto productToUpdate) =>
+            _mapper.Map<ProductDto>(await _ops.UpdateEntityOperation(e => e.UID == id, _mapper.Map<Product>(productToUpdate)));
     }
 }
