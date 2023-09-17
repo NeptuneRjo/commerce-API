d44764003bce85341013fae284f844e2835a7fb7
NeptuneRjo <rjo9000@outlook.com>
2023-09-17 02:16:43 -0400

    Update service tests to operations tests

---
+++ CommerceApi.Test/Operations/ProductOperationTests.cs
@@ -0,0 +1,40 @@
+﻿using CommerceApi.BLL.Utilities;
+using CommerceApi.DAL.Repositories;
+using CommerceApi.Test.Utilities;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit.Abstractions;
+
+namespace CommerceApi.Test.Operations
+{
+    public class ProductOperationTests : TestUtilities
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IProductRepository _repository;
+        private readonly ILogger<ProductOperations> _logger;
+
+        private readonly ProductEntityUtilities _utils;
+
+        private readonly IProductOperations _ops;
+
+        public ProductOperationTests(ITestOutputHelper output)
+        {
+            _output = output;
+            _repository = Substitute.For<IProductRepository>();
+            _logger = Substitute.For<ILogger<ProductOperations>>();
+            _utils = new ProductEntityUtilities();
+            _ops = new ProductOperations(_repository, _logger);
+        }
+
+        [Fact]
+        public async Task AddProductOperation_When_Success_ReturnsProduct()
+        {
+            _repository.Add(_utils.ProductEntity).Returns(Task.FromResult(_utils.ProductEntity));
+
+            var result = await _ops.AddProductOperation(_utils.ProductEntity);
+
+            Assert.Equivalent(result, _utils.ProductEntity, strict: true);
+        }
+    }
+}
