aaaea7a2c3380ee0350b802fe4ae4a885812425a
NeptuneRjo <rjo9000@outlook.com>
2023-09-17 23:16:45 -0400

    Create dto for adding cartproduct

---
+++ CommerceApi.DTO/DTOS/Request/CartProductToAddDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CommerceApi.DTO.DTOS
+{
+    public class CartProductToAddDto
+    {
+        public string CartId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
