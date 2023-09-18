3cca643aa6be790926717e585d7c09bc096035d4
NeptuneRjo <rjo9000@outlook.com>
2023-09-18 16:26:25 -0400

    Define cart product service interface

---
+++ CommerceApi.BLL/Services/IServices/ICartProductService.cs
@@ -0,0 +1,16 @@
+﻿using CommerceApi.DAL.Entities;
+using CommerceApi.DTO.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceApi.BLL.Services
+{
+    public interface ICartProductService : IGenericService<CartProduct>
+    {
+        Task AddCartProductAsync(string cartId, CartProductToAddDto productToAdd);
+
+    }
+}
