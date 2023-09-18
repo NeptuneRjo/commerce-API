b081f31ae5b33c754457aadf527068ab9c5bae75
NeptuneRjo <rjo9000@outlook.com>
2023-09-18 16:25:13 -0400

    Define cart product operations

---
+++ CommerceApi.BLL/Utilities/Operations/CartProductOperations.cs
@@ -0,0 +1,25 @@
+﻿using CommerceApi.DAL.Entities;
+using CommerceApi.DAL.Repositories;
+using CommerceApi.DTO.DTOS;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommerceApi.BLL.Utilities.Operations
+{
+    public class CartProductOperations : GenericOperations<CartProduct>, ICartProductOperations
+    {
+        private readonly ICartProductRepository _repository;
+        private readonly ILogger<CartProductOperations> _logger;
+
+        public CartProductOperations(ICartProductRepository repository, ILogger<CartProductOperations> logger) : base(repository, logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+    }
+}
---
+++ CommerceApi.BLL/Utilities/Operations/IOperations/ICartProductOperations.cs
@@ -0,0 +1,9 @@
+﻿using CommerceApi.DAL.Entities;
+using CommerceApi.DTO.DTOS;
+
+namespace CommerceApi.BLL.Utilities.Operations
+{
+    public interface ICartProductOperations : IGenericOperations<CartProduct>
+    {
+    }
+}
