81a241812e8e0a0d3ce9341246a10f74599202af
NeptuneRjo <rjo9000@outlook.com>
2023-09-17 23:49:05 -0400

    remove unused dependencies from product operations

--- CommerceApi.BLL/Utilities/Operations/ProductOperations.cs
+++ CommerceApi.BLL/Utilities/Operations/ProductOperations.cs
@@ -1,12 +1,6 @@
 ﻿using CommerceApi.DAL.Entities;
 using CommerceApi.DAL.Repositories;
-using CommerceApi.DTO.DTOS;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommerceApi.BLL.Utilities
 {
